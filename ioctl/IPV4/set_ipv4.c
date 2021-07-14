07fc46a433426dc28c2150952e50809d73d7a24f
yangchao <yangchao_nick@163.com>
2021-07-14 11:38:42 -0400

    commit add route source code

--- ioctl/IPV4/set_ipv4.c
+++ ioctl/IPV4/set_ipv4.c
@@ -11,47 +11,125 @@
 #include <fcntl.h> 
 #include <unistd.h>
 #include <errno.h>
+#include <net/route.h>
+
 
 #define SET_DEV_UP 1
 #define SET_DEV_DOWN 0
-/*
+
+static int set_ipv4_gateway(char *dev, char *gateway)
+{
+    int sockfd = 0;;
+    int rv = 0;
+    char iface[128] = {0};
+    struct rtentry rt;
+    struct sockaddr_in sockaddr;
+    
+    memset(&rt, 0, sizeof(rt));
+    memset(&sockaddr, 0, sizeof(sockaddr));
+    strcpy(iface, dev);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0){
+        return -1;
+    }
+ 
+    sockaddr.sin_family = AF_INET;
+    rv = inet_pton(AF_INET, gateway, &(sockaddr.sin_addr));
+    if (rv != 1){
+        fprintf(stderr, "[Error]:Invalid gateway: %s\n", gateway);
+        close(sockfd);
+        return  -1;
+    }
+
+    memcpy (&rt.rt_gateway, &sockaddr, sizeof(struct sockaddr_in));
+    rt.rt_dst.sa_family = AF_INET;
+    rt.rt_genmask.sa_family = AF_INET;
+    rt.rt_flags = RTF_GATEWAY;
+    rt.rt_dev = iface;
+
+    rv = ioctl(sockfd, SIOCADDRT, &rt);
+    if (rv != 0){
+        fprintf(stderr, "[Error]:SIOCADDRT: %s\n",strerror(errno));
+        close(sockfd);
+        return rv;
+    }
+
+   close(sockfd);
+   return rv;
+}
+
 static int get_ipv4_ip_mask(char* dev, char *addr, char *mask)
 {
     int rv = 0;
     int sockfd = 0;
+    char ip[128] = {0};
+    char ip_mask[128] = {0};
     struct sockaddr_in *sin;
-    char *ip = NULL;
     struct ifreq ifr;
-    memset(&ifr, 0, sizeof(ifr));
+
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         return -1;
     }
- 
+    
+    // get ipv4 ip
+    memset(&ifr, 0, sizeof(ifr));
     strcpy(ifr.ifr_name,dev);
-    rv = ioctl(sockfd, action, &ifr);
-    if(rv == 0){
-        sin = (struct sockaddr_in *)&ifr.ifr_addr;
-        ip = inet_ntoa(sin->sin_addr);
-        strcpy(addr, ip);
+    rv = ioctl(sockfd, SIOCGIFADDR, &ifr);
+    if (rv != 0){
+        fprintf(stderr, "[Error]:SIOCGIFADDR: %s\n",strerror(errno));
+        close(sockfd);
+        return -1;
     }
+    sin = (struct sockaddr_in *)&(ifr.ifr_netmask);
+    inet_ntop(AF_INET, &(sin->sin_addr), ip, sizeof(ip));
+    if (ip[0] == '\0'){
+        fprintf(stderr, "inet_ntop converted ip error\n");
+        close(sockfd);
+        return  -1;
+    }
+    strcpy(addr, ip);
+    
+
+    // get ipv4 mask
+    memset(&ifr, 0, sizeof(ifr));
+    strcpy(ifr.ifr_name,dev);
+    rv = ioctl(sockfd, SIOCGIFNETMASK, &ifr);
+    if (rv != 0){
+        fprintf(stderr, "[Error]:SIOCGIFNETMASK: %s\n",strerror(errno));
+        close(sockfd);
+        return -1;
+    }
+    sin = (struct sockaddr_in *)&(ifr.ifr_addr);
+    inet_ntop(AF_INET, &(sin->sin_addr), ip_mask, sizeof(ip_mask));
+    if (ip_mask[0] == '\0'){
+        fprintf(stderr, "inet_ntop converted ip error\n");
+        close(sockfd);
+        return -1;
+    }
+    strcpy(mask, ip_mask);
 
     close(sockfd);
     return rv;
 }
-*/
+
 static int set_iface_flag(char * dev, int action)
 {
     int rv = 0;
-    int sockfd;
-    int flag;
+    int sockfd = 0;
+    int flag = 0;
     struct ifreq ifr;
 
     memset(&ifr, 0, sizeof(ifr));
-    if((sockfd = socket(PF_INET,SOCK_STREAM,0)) < 0){
+    if((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0){
         return -1;
     }
     strcpy(ifr.ifr_name,dev);
     rv = ioctl(sockfd,SIOCGIFFLAGS,&ifr);
+    if (rv != 0){
+        fprintf(stderr, "[Error]:SIOCGIFFLAGS: %s\n",strerror(errno));
+        close(sockfd);
+        return 1;
+    }
     if(action == SET_DEV_DOWN){
         flag = ~IFF_UP;
         ifr.ifr_flags &= flag;
@@ -60,9 +138,12 @@ static int set_iface_flag(char * dev, int action)
         ifr.ifr_flags |= flag;
     }
     rv = ioctl(sockfd,SIOCSIFFLAGS,&ifr);
-    if(rv != 0){
-        printf("ioctl error \n");
+    if (rv != 0){
+        fprintf(stderr, "[Error]:SIOCSIFFLAGS: %s\n",strerror(errno));
+        close(sockfd);
+        return 1;
     }
+    close(sockfd);
     return rv;
 }
 
@@ -74,36 +155,50 @@ static int set_ipv4_addr_mask(char *dev, char *addr, char *mask)
     struct sockaddr_in sin;
 
     if(addr[0] == '\0' || mask[0] == '\0'){
-        fprintf(stdout, "[error]: Invalid parameter\n");
+        fprintf(stderr, "[Error]: Invalid add or mask\n");
         return -1;
     }
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        return -1;
+    }
 
     // set ipv4 addr
     memset(&ifr, 0, sizeof(ifr));
     memset(&sin, 0, sizeof(sin));
     strcpy(ifr.ifr_name, dev);
     sin.sin_family = AF_INET;
-    inet_pton(AF_INET, addr, &(sin.sin_addr));
+    rv = inet_pton(AF_INET, addr, &(sin.sin_addr));
+    if (rv != 1){
+        fprintf(stderr, "Invalid addr: %s\n",addr);
+        close(sockfd);
+        return -1;
+    }
     memcpy(&ifr.ifr_addr, &sin, sizeof(struct sockaddr));
     rv = ioctl(sockfd, SIOCSIFADDR, &ifr);
-    printf("SIOCSIFADDR rv:%d\n",rv);
-    /*
-    if((ioctl(sockfd, SIOCSIFADDR, &ifr)) != 0){
-        fprintf(stderr, "[error]:SIOCSIFADDR: %s\n",strerror(errno));
+    if (rv != 0){
+        fprintf(stderr, "[Error]:SIOCSIFADDR: %s\n",strerror(errno));
+        close(sockfd);
         return 1;
     }
-    */
 
     // set ipv4 mask
     memset(&ifr, 0, sizeof(ifr));
     memset(&sin, 0, sizeof(sin));
     strcpy(ifr.ifr_name, dev);
     sin.sin_family = AF_INET;
-    inet_pton(AF_INET, mask, &(sin.sin_addr));
+    rv = inet_pton(AF_INET, mask, &(sin.sin_addr));
+    if (rv != 1){
+        fprintf(stderr, "Invalid mask: %s\n",mask);
+        close(sockfd);
+        return -1;
+    }
     memcpy(&ifr.ifr_netmask, &sin, sizeof(struct sockaddr));
     rv = ioctl(sockfd, SIOCSIFNETMASK, &ifr);
-    printf("SIOCSIFNETMASK rv:%d\n",rv);
+    if (rv != 0){
+        fprintf(stderr, "[Error]:SIOCSIFADDR: %s\n",strerror(errno));
+        close(sockfd);
+        return 1;
+    }
     close(sockfd);
     return 0;
 }
@@ -112,19 +207,21 @@ static int set_ipv4_addr_mask(char *dev, char *addr, char *mask)
 
 int main()
 {
-    int rv = 0;
     char ethname[] = "docker0";
-    char ipv4_addr[] = {0};
+    char ipv4_addr[] = "172.21.24.13";
     char ipv4_mask[] = "255.255.0.0";
-    char ip[125] = {0};
-    char mask[125] = {0};
+    char gateway[] = "172.17.1.6";
+    char ip[128] = {0};
+    char mask[128] = {0};
 
     //set_iface_flag(ethname, SET_DEV_UP);
-    set_ipv4_addr_mask(ethname, "10.70.74.11", "255.255.24a0.0");
 
+    //set_ipv4_addr_mask(ethname, ipv4_addr, ipv4_mask);
 
-    //get_ipv4_ip_mask(ethname, ip, mask)
-    //get_addr(ethname, ipv4_addr, SIOCGIFADDR);
-    //get_addr(ethname, ipv4addr_mask, SIOCGIFNETMASK);
+    //get_ipv4_ip_mask(ethname, ip, mask);
+
+    //set_ipv4_gateway(ethname, gateway);
+
+    return 0;
 
 }
